54cfb0a36f5067ebd66b59186de3473e315ced89
Andrew T <act.webdedigns@gmail.com>
2018-01-17 23:12:35 -0600

    TODO: twitter function

--- liri.js
+++ liri.js
@@ -1,50 +1,52 @@
-var dotenv = require('dotenv').config({path:'./keys.js'}); 
+const dotenv = require('dotenv').config();
 if (dotenv.err) {
-throw dotenv.err}
-const buf = new Buffer('BASIC=basic')
-var config = dotenv.parse(buf)
-console.log(process.env)
-const fs = require('fs')
-const request = require('request');
-const spotify = require('spotify')
-/*const spotifyApi = new SpotifyWebApi({*/
-    //clientId: process.env.SPOTIFY_ID,
-    //clientSecret: process.env.SPOTIFY_SECRET
-//});
+  throw dotenv.err;
+}
+const env = dotenv.parsed;
 
+const fs = require('fs');
+const request = require('request');
+
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify({
+  id: env.SPOTIFY_ID,
+  secret: env.SPOTIFY_SECRET,
+});
 const Twitter = require('twitter');
-const twitter = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
+const client = new Twitter({
+  consumer_key: env.TWITTER_CONSUMER_KEY,
+  consumer_secret: env.TWITTER_CONSUMER_SECRET,
+  access_token_key: env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 
 const program = process.argv[2];
 const command = process.argv[3];
+//console.log(env)
 
 // Switch Statement
 switch (program) {
-    case 'movie-this':
-        movieSearch(command);
-        break;
-    case 'spotify-this-song':
-        spotifySearch(command)
-        break;
-    case 'my-tweets':
-        postTwitter(command)
-        break;
+  case 'movie-this':
+    movieSearch(command);
+    break;
+  case 'spotify-this-song':
+    spotifySearch(command);
+    break;
+  case 'my-tweets':
+    postTwitter(command);
+    break;
 }
 
 // Movie Search function
 function movieSearch(command) {
-    let queryUrl = 'http://www.omdbapi.com/?t=' + command + '&y=&plot=short&apikey=trilogy';
-    request(queryUrl, (err, res, body) => {
-        if (err) {
-            console.log(err);
-        }
-        let data = JSON.parse(body);
-        console.log(`
+  let queryUrl =
+    'http://www.omdbapi.com/?t=' + command + '&y=&plot=short&apikey=trilogy';
+  request(queryUrl, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+    let data = JSON.parse(body);
+    console.log(`
 Title: ${data.Title} (${data.Year}) ${data.Rated}
 Genre: ${data.Genre}  Runtime: ${data.Runtime}
 
@@ -54,22 +56,43 @@ ${data.Plot}
 Starring: ${data.Actors}
 Directed by ${data.Director}
 Written by ${data.Writer}`);
-    });
+  });
 }
 
 // Spotify search funtion
 function spotifySearch(command) {
-    spotify.search({ type: 'track', query: command, limit: 20, id: spotify.id}, (err, data) => {
-       if ( err ) {
-        console.log('Error occurred: ' + err)
-        return
-       } 
-        console.log(data)
-    })
- }  
+  if (command === undefined) {
+    command = 'Never gonna give you up';
+  }
+  spotify.search(
+    {
+      type: 'track',
+      query: command,
+      limit: 3
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      let songs = data.tracks.items;
+      for (song in songs) {
+        let s = songs[song];
+        let a = []
+        for (artist in s.artists){
+          let name = s.artists[artist].name 
+          a.push(name)
+        }
+        console.log(s);
+        console.log(` 
+song name: ${s.name} 
+artist(s): ${JSON.stringify(a, 2)}
 
-// Twitter post funtion
-function postTwitter(command) {
-    console.log(process.env)
-console.log(config)
+preview song: ${s.preview_url}
+            `)
+      }
+    }
+  );
 }
+// Twitter post funtion
+function postTwitter(command) {}
